79699ed252aaae2df3126a37170c3924d8cb2bb7
Turtleca <sleepingturtle294@gmail.com>
2025-11-08 01:49:42 -0700

    initial commit

--- src/racecourse.py
+++ src/racecourse.py
@@ -2,10 +2,24 @@
 Race course Parameters class
 """
 
+from enum import Enum
+
+# TODO:
+# - Store the course parameters for a specific race
+# - check the course id based on name and distance
+# - export in a bash readable way
+
 
 class RaceCourse:
-    course: str
-    dist: int
-    weather: str
-    ground: str
-    season: str
+
+    def __init__(
+        self, track: str, distance: int, weather: str, ground: str, season: str
+    ):
+        self.track = track
+        self.distance = int(distance)
+        self.weather = weather
+        self.ground = ground
+        self.season = season
+
+    def set_track(self):
+        pass
